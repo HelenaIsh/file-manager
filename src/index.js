064e112d8a7b943e7719650aad6b192cf42e3a43
HelenaIsh <elena-ishmatova@mail.ru>
2024-10-11 10:32:49 +0200

    feat: add consoling homedir

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import readline from 'readline';
+import os from 'os';
 
 const userName = process.argv.slice(2)[0].split('=')[1] ?? 'Anonymous';
+let currentDir = os.homedir()
 
 console.log(`Welcome to the File Manager, ${userName}`);
+console.log(`You are currently in ${currentDir}`);
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -13,6 +16,8 @@ rl.on('line', async (input) => {
   input = input.trim();
   if (input === '.exit') {
     rl.close();
+  } else {
+    console.log(`You are currently in ${currentDir}`);
   }
 });
 
