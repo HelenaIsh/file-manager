400c934eb0b2623545e5fa43d3472333b4a3ec2a
HelenaIsh <elena-ishmatova@mail.ru>
2024-10-11 16:52:51 +0200

    fix: fix error in cp

--- src/commands/cp.js
+++ src/commands/cp.js
@@ -4,7 +4,7 @@ import { pipeline } from 'node:stream/promises';
 
 const copyFile = async (currentDir, fileName, newFileDirectory) => {
     const readableStream = fs.createReadStream(path.resolve(currentDir, fileName));
-    const writeableStream = fs.createReadStream(path.resolve(currentDir, newFileDirectory, fileName));
+    const writeableStream = fs.createWriteStream(path.resolve(currentDir, newFileDirectory, fileName));
     await pipeline(readableStream, writeableStream);
     return currentDir;
 }
