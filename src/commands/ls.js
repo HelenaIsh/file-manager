c22b52a2bdf01a7343b85320846eab64f545ea74
HelenaIsh <elena-ishmatova@mail.ru>
2024-10-11 14:05:56 +0200

    feat: add sorting

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -2,6 +2,15 @@ import fs from 'node:fs/promises';
 
 const listDirectory = async (currentDir) => {
     const files = await fs.readdir(currentDir, { withFileTypes: true });
+    files.sort((a, b) => {
+        if (a.isDirectory() && !b.isDirectory()) {
+          return -1;
+        }
+        if (!a.isDirectory() && b.isDirectory()) {
+          return 1;
+        }
+        return a.name.localeCompare(b.name);
+      });
     console.table(files.map((file) => ({
         Name: file.name,
         Type: file.isDirectory() ? 'directory' : 'file'
