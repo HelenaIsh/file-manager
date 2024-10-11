8884385d4eec36c81ff763c411e9a75e4d92a6dc
HelenaIsh <elena-ishmatova@mail.ru>
2024-10-11 10:20:50 +0200

    feat: add readline and closing handler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-const userName = process.argv.slice(2)[0].split('=')[1];
+import readline from 'readline';
+
+const userName = process.argv.slice(2)[0].split('=')[1] ?? 'Anonymous';
 
 console.log(`Welcome to the File Manager, ${userName}`);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+rl.on('line', async (input) => {
+  input = input.trim();
+  if (input === '.exit') {
+    rl.close();
+  }
+});
+
+rl.on('close', () => {
+  console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+  process.exit(0);
+});
